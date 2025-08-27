f31e41b7a8bce6db009ba0acf12a406d8ab76683
kappa <kappa@entropi.kr>
2025-08-27 15:39:16 +0900

    base vm

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,7 @@
 use crate::{__kernel_base, __stack_top, ld_variable, println};
+use bitflags::bitflags;
 use core::fmt::{Formatter, LowerHex, UpperHex};
-use core::ops::{Add, AddAssign, Range};
+use core::ops::{Add, AddAssign, Deref, Range};
 use core::ptr;
 
 macro_rules! impl_addr {
@@ -83,7 +84,7 @@ impl Region {
     }
 }
 
-const PAGE_SIZE: usize = 4096;
+pub const PAGE_SIZE: usize = 4096;
 static mut CURRENT_REGION: Option<Region> = None;
 static mut NEXT_PAGE_ADDR: PAddr = PAddr::zero();
 
@@ -115,7 +116,8 @@ fn exclude_range_from_range<T: PartialOrd + Copy>(
 ///
 /// currently this function simply selects the largest region
 pub fn set_region_from_fdt() {
-    let kernel_reserved_range = unsafe { ld_variable!(__kernel_base, u8)..ld_variable!(__stack_top, usize) };
+    let kernel_reserved_range =
+        unsafe { ld_variable!(__kernel_base, u8)..ld_variable!(__stack_top, usize) };
     let mut largest_region: Option<Region> = None;
     for region in crate::dtb::fdt().memory().regions() {
         let base = (region.starting_address as usize)
@@ -163,9 +165,67 @@ pub fn allocate(n: usize) -> PAddr {
             panic!("out of current memory region");
         }
 
-        println!("writing zeros for created page at {addr:#x}");
         ptr::write_bytes(addr.as_mut_ptr(), 0, n * PAGE_SIZE / size_of::<u8>());
     }
 
     addr
+}
+
+const SATP_SV32: usize = 1usize << 31;
+
+bitflags! {
+    #[derive(Copy, Clone)]
+    pub struct PageFlag: usize {
+        const Valid = 1 << 0;
+        const Read = 1 << 1;
+        const Write = 1 << 2;
+        const Execute = 1 << 3;
+        const User = 1 << 4;
+
+        const ReadWriteExecute = Self::Read.bits() | Self::Write.bits() | Self::Execute.bits();
+    }
+}
+
+/// Sv32: VPN1(10bits) + VPN2(10bits) + Offset(12bits)
+pub fn map_page_sv32(table1: *mut usize, vaddr: VAddr, paddr: PAddr, flags: PageFlag) {
+    if !unsafe { vaddr.as_ptr() }.is_aligned() {
+        panic!("unaligned vaddr {vaddr:#x}");
+    };
+    if !unsafe { paddr.as_ptr() }.is_aligned() {
+        panic!("unaligned paddr {paddr:#x}");
+    };
+
+    let vpn1 = (vaddr.addr() >> 22) & 0x3ff;
+    if unsafe { *table1.offset(vpn1 as isize) } & PageFlag::Valid.bits() == 0 {
+        println!("allocate directory on {:?}", unsafe { table1.offset(vpn1 as isize) });
+        // allocate a page for root page table
+        let page_table = allocate(1);
+        unsafe {
+            table1
+                .offset(vpn1 as isize)
+                .write(((page_table.addr() / PAGE_SIZE) << 10) | PageFlag::Valid.bits())
+        }
+    }
+
+    // set up 2nd level page table
+    let vpn0 = (vaddr.addr() >> 12) & 0x3ff;
+    let table0 = ((unsafe { *table1.offset(vpn1 as isize) } >> 10) * PAGE_SIZE) as *mut usize;
+    unsafe {
+        table0
+            .offset(vpn0 as isize)
+            .write(((paddr.addr() / PAGE_SIZE) << 10) | flags.bits() | PageFlag::Valid.bits())
+    }
+}
+
+pub fn map_page_to_heap(
+    table: *mut usize,
+    flags: PageFlag,
+    mapper: fn(*mut usize, VAddr, PAddr, PageFlag),
+) {
+    #[allow(static_mut_refs)]
+    let region = unsafe { CURRENT_REGION.as_ref().unwrap() };
+    for paddr in (region.addr.addr()..(region.addr.addr() + region.size)).step_by(PAGE_SIZE) {
+        // println!("[map_page_to_heap#] map {paddr:#x}");
+        mapper(table, VAddr(paddr), PAddr(paddr), flags)
+    }
 }
