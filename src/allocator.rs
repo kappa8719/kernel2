74a60f86e242062f2cf1816e761fab6c731234a8
kappa <kappa@entropi.kr>
2025-09-01 01:09:45 +0900

    finish allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,9 +1,13 @@
 use core::{
     alloc::{GlobalAlloc, Layout},
     fmt::{self, Debug},
+    ops::Add,
 };
 
-use crate::{memory::Region, println};
+use crate::{
+    memory::{PAddr, Region},
+    println,
+};
 
 #[derive(Debug)]
 struct LinkedNode<T = u16> {
@@ -75,6 +79,7 @@ const MAXIMUM_ORDER: usize = 10;
 const MAXIMUM_BLOCK: usize = MINIMUM_BLOCK * 2usize.pow(MAXIMUM_ORDER as u32);
 
 pub struct BuddyAllocator {
+    region: Region,
     free_lists: *mut [LinkedNode],
     metadata: *mut [Metadata],
     links: *mut [LinkedNode],
@@ -98,6 +103,10 @@ impl BuddyAllocator {
     /// Creates an uninitialized BuddyAllocator
     const unsafe fn null() -> Self {
         Self {
+            region: Region {
+                addr: PAddr::zero(),
+                size: 0,
+            },
             free_lists: core::ptr::slice_from_raw_parts_mut(core::ptr::null_mut(), 0),
             metadata: core::ptr::slice_from_raw_parts_mut(core::ptr::null_mut(), 0),
             links: core::ptr::slice_from_raw_parts_mut(core::ptr::null_mut(), 0),
@@ -152,6 +161,7 @@ impl BuddyAllocator {
             }
 
             Self {
+                region,
                 free_lists,
                 metadata,
                 links,
@@ -168,7 +178,7 @@ impl BuddyAllocator {
 
         unsafe {
             heap = heap.add(heap.align_offset(align_of::<LinkedNode>()));
-            heap = heap.add(size_of::<LinkedNode>() * orders); // free list array
+            heap = heap.add(size_of::<LinkedNode>() * (orders + 1)); // free list array
 
             heap = heap.add(heap.align_offset(align_of::<Metadata>()));
             heap = heap.add(size_of::<Metadata>() * subranges); // metadata array
@@ -180,18 +190,19 @@ impl BuddyAllocator {
         heap.addr()
     }
 
-    pub fn allocate_unchecked(&self, size: usize) -> *mut u8 {
+    pub unsafe fn allocate_unchecked(&self, size: usize) -> *mut u8 {
         let desired_order = (size.ilog2() - MINIMUM_BLOCK.ilog2()) as usize;
 
         // find free block of at least requested size
-        let found_order = desired_order;
+        let mut pool = desired_order;
         let mut block = u16::MAX;
-        for pool in desired_order..self.orders {
+        while pool <= self.orders {
             let node = unsafe { &(*self.free_lists)[pool] };
             if node.head != u16::MAX {
                 block = node.head;
                 break;
             }
+            pool += 1;
         }
 
         // return null if not found
@@ -203,34 +214,112 @@ impl BuddyAllocator {
         *metadata = metadata.with_is_free_list(false);
 
         // split unused buddies and add them to free lists
-        for pool in (desired_order..found_order).rev() {
-            let buddy = block ^ (1 << found_order);
+        while pool > desired_order {
+            pool -= 1;
+            let buddy = block ^ (1 << pool);
             let buddy_metadata = unsafe { &mut (*self.metadata)[buddy as usize] };
             *buddy_metadata = Metadata::new(false, true, pool as u8);
+            self.add_to_free_list(buddy as usize, *buddy_metadata);
         }
 
-        unimplemented!()
+        *metadata = Metadata::new(true, false, pool as u8);
+
+        let addr = (block << MINIMUM_BLOCK.ilog2()) as usize;
+        unsafe { self.region.addr.as_mut_ptr().add(addr) }
+    }
+
+    pub unsafe fn free_unchecked(&self, ptr: *mut u8) {
+        let offset = ptr.addr() - self.region.addr.addr();
+        let block = offset >> MINIMUM_BLOCK.ilog2();
+        let metadata = unsafe { &mut (*self.metadata)[block] };
+        let pool = metadata.pool();
+
+        *metadata = metadata.with_is_allocated(false);
+
+        // the block updates as it merges with its buddy
+        let mut block = block;
+        let mut pool = pool as usize;
+        while pool < self.orders {
+            let buddy = block ^ (1 << pool);
+            let buddy_metadata = unsafe { &mut (*self.metadata)[buddy] };
+            if !buddy_metadata.is_free_list() || buddy_metadata.pool() < (pool as u8) {
+                // no buddy is allocated
+                break;
+            }
+
+            *buddy_metadata = buddy_metadata.with_is_free_list(false);
+            self.remove_from_free_list(buddy, *buddy_metadata);
+            block &= (u16::MAX << (pool + 1)) as usize;
+            pool += 1;
+        }
+
+        // update merged block (if done)
+        let metadata = unsafe { &mut (*self.metadata)[block] };
+        *metadata = Metadata::new(false, true, pool as u8);
+        self.add_to_free_list(block, *metadata);
+    }
+
+    fn add_to_free_list(&self, block: usize, metadata: Metadata) {
+        let link = unsafe { &mut (*self.links)[block] };
+        let free_list = unsafe { &mut (*self.free_lists)[metadata.pool() as usize] };
+
+        let head = free_list.head;
+        free_list.head = block as u16;
+        if head == u16::MAX {
+            free_list.tail = block as u16;
+        } else {
+            link.tail = head;
+            unsafe { &mut (*self.links)[head as usize] }.head = block as u16;
+        }
+    }
+
+    fn remove_from_free_list(&self, block: usize, metadata: Metadata) {
+        let link = unsafe { &mut (*self.links)[block] };
+        let free_list = unsafe { &mut (*self.free_lists)[metadata.pool() as usize] };
+
+        if link.head == u16::MAX {
+            free_list.head = link.tail;
+        } else {
+            let head_link = unsafe { &mut (*self.links)[link.head as usize] };
+            head_link.tail = link.tail;
+        }
+
+        if link.tail == u16::MAX {
+            free_list.tail = link.head;
+        } else {
+            let tail_link = unsafe { &mut (*self.links)[link.tail as usize] };
+            tail_link.head = link.head;
+        }
+
+        *link = LinkedNode {
+            head: u16::MAX,
+            tail: u16::MAX,
+        };
     }
 }
 
 unsafe impl GlobalAlloc for BuddyAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        todo!()
+        let mut size = layout.size().next_power_of_two();
+        if size < MINIMUM_BLOCK {
+            size = MINIMUM_BLOCK;
+        }
+        unsafe { self.allocate_unchecked(size) }
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        todo!()
+    unsafe fn dealloc(&self, ptr: *mut u8, _: Layout) {
+        unsafe {
+            self.free_unchecked(ptr);
+        }
     }
 }
 
-static mut ALLOCATOR: BuddyAllocator = unsafe { BuddyAllocator::null() };
+#[global_allocator]
+pub static mut ALLOCATOR: BuddyAllocator = unsafe { BuddyAllocator::null() };
 
 /// Initialize global allocator
 pub unsafe fn initialize_global(region: Region, heap: &mut [u8]) {
     unsafe {
         ALLOCATOR = BuddyAllocator::new(region, heap);
-        #[allow(static_mut_refs)]
-        let allocator = &ALLOCATOR;
-        println!("{allocator:#?}");
     }
 }
